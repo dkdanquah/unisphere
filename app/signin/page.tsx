6bfed012f4c254592c256238a205a843b40b775e
Derrick <dkdanquah001@st.ug.edu.gh>
2023-07-20 18:12:37 +0000

    fix: fix signin view

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:px-0">
+      <div className="container relative grid h-[800px] max-w-none flex-col items-center justify-center px-5 lg:px-0">
         <Link
           href="/"
           className={cn(
